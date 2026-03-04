ceae3a465e359e1564c718132df313176096a168
dhiraj <dhirajmymodak22@gmail.com>
2026-03-04 17:39:35 +0530

    product image upload middleware added

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,3 +1,4 @@
+const upload = require("../config/multerConfig");
 const {
   createProduct,
   updateProduct,
@@ -9,8 +10,22 @@ const {
 
 const productRouter = require("express").Router();
 
-productRouter.post("/add", createProduct);
-productRouter.put("/update/:id", updateProduct);
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "images", maxCount: 10 },
+    { name: "guideImage", maxCount: 1 },
+  ]),
+  createProduct,
+);
+productRouter.put(
+  "/update/:id",
+  upload.fields([
+    { name: "images", maxCount: 10 },
+    { name: "guideImage", maxCount: 1 },
+  ]),
+  updateProduct,
+);
 productRouter.get("/get-by-collection/:collectionId", getProductsBycollections);
 productRouter.get("/get-by-category/:categoryId", getProductsByCategories);
 productRouter.get("/get-by-id/:id", getProductById);
